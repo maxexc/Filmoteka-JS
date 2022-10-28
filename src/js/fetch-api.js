63da163afeb26068b9ea8505ee22d70741112ae9
Maksym Solomko <max.exc7@gmail.com>
2022-10-29 02:31:11 +0300

    test

---
+++ src/js/fetch-api.js
@@ -0,0 +1,5 @@
+// fetch(
+//         https://api.themoviedb.org/3/trending/movie/day?api_key=2f44dbe234f7609a16da7327d83f3eb3&page=1&language=${currentLang}
+//     )
+//         .then(res => res.json())
+//         .then(data =>  console.log(data))
