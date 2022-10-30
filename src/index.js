217ffb5ccb264ce4e17a58fba58c6939d8b4d400
Maksym Solomko <max.exc7@gmail.com>
2022-10-31 00:07:16 +0200

    up

--- src/index.js
+++ src/index.js
@@ -27,66 +27,6 @@ function onUkClick() {
 const currentLanguage = localStorage.getItem('language');
 console.log(currentLanguage);
 
-// fetchGenres(currentLanguage);
-// async function fetchGenres(language) {
-//   const currentLang = language;
-//   await fetch(
-//     `https://api.themoviedb.org/3/genre/movie/list?api_key=f87210516a7f6fda7a5c975f08793382&language=${currentLang}`
-//   )
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(async data => {
-//       console.log(data.genres);
-//       let genres = await data.genres;
-//       console.log(genres);
-//       return genres;
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
-
-// // console.log(genres);
-
-// const genres = [
-//   { id: 28, name: 'Action' },
-//   { id: 12, name: 'Adventure' },
-//   { id: 16, name: 'Animation' },
-//   { id: 35, name: 'Comedy' },
-//   { id: 80, name: 'Crime' },
-//   { id: 99, name: 'Documentary' },
-//   { id: 18, name: 'Drama' },
-//   { id: 10751, name: 'Family' },
-//   { id: 14, name: 'Fantasy' },
-//   { id: 36, name: 'History' },
-//   { id: 27, name: 'Horror' },
-//   { id: 10402, name: 'Music' },
-//   { id: 9648, name: 'Mystery' },
-//   { id: 10749, name: 'Romance' },
-//   { id: 878, name: 'Science Fiction' },
-//   { id: 10770, name: 'TV Movie' },
-//   { id: 53, name: 'Thriller' },
-//   { id: 10752, name: 'War' },
-//   { id: 37, name: 'Western' },
-// ];
-
-// // let ids = 37;
-// // console.log(ids);
-
-// const searchGenresName = ids => {
-//   let genresNamesArr = [];
-//   const searchId = ids;
-//   // console.log(searchId);
-
-//   for (var i = 0; i < ids.length; i++) {
-//     elGenreName = genres.find(list => list.id === searchId[i]).name;
-//     genresNamesArr.push(elGenreName);
-//     // continue;
-//   }
-//   return genresNamesArr;
-// };
-
 fetchApi(currentLanguage);
 function fetchApi(language) {
   const currentLang = language;
@@ -109,7 +49,7 @@ function fetchApi(language) {
     <p class='text'>${item.genre_ids} | ${item.release_date}</p>
   </div>
    <div class="over">
-    <p>${item.overview}</p>
+    <p></p>
     </div>
                         <div class="rating">
                         <span class="rating-value">${item.vote_average.toFixed(
