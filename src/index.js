8e8b41283bcedec612cce0e91a9a8ac0a7fdbe66
Maksym Solomko <max.exc7@gmail.com>
2022-11-01 18:46:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,6 +41,28 @@ async function fetchGenres(language) {
 
 // console.log(genres);
 
+const genres = [
+  { id: 28, name: 'Action' },
+  { id: 12, name: 'Adventure' },
+  { id: 16, name: 'Animation' },
+  { id: 35, name: 'Comedy' },
+  { id: 80, name: 'Crime' },
+  { id: 99, name: 'Documentary' },
+  { id: 18, name: 'Drama' },
+  { id: 10751, name: 'Family' },
+  { id: 14, name: 'Fantasy' },
+  { id: 36, name: 'History' },
+  { id: 27, name: 'Horror' },
+  { id: 10402, name: 'Music' },
+  { id: 9648, name: 'Mystery' },
+  { id: 10749, name: 'Romance' },
+  { id: 878, name: 'Science Fiction' },
+  { id: 10770, name: 'TV Movie' },
+  { id: 53, name: 'Thriller' },
+  { id: 10752, name: 'War' },
+  { id: 37, name: 'Western' },
+];
+
 // let ids = 37;
 // console.log(ids);
 
@@ -83,7 +105,9 @@ async function fetchApi(language) {
             />
             <div class="card-box">
               <h3 class="card-title">${item.title}</h3>
-              <p class="text">${item.genre_ids} | ${item.release_date}</p>
+              <p class="text">${searchGenresName(item.genre_ids)} | ${
+            item.release_date
+          }</p>
             </div>
             <div class="over">
               <p>${item.overview}</p>
