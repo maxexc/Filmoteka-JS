11e5ee94d800073e185c88beeee96bcccd0d4475
Maksym Solomko <max.exc7@gmail.com>
2022-11-02 00:38:53 +0200

    up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// import genresAll from './js/genres.js';
+import genresAll from './js/genres.js';
 // import movieCard from './templates/card.hbs';
 
-// console.log(genresAll);
-// console.log(genresAll.genresUkr);
+console.log(genresAll);
+console.log(genresAll.genresUkr);
 
 // console.dir(movieCard());
 
@@ -13,51 +13,51 @@ const btnUk = document.querySelector('#uk');
 btnEn.addEventListener('click', onEnClick);
 btnUk.addEventListener('click', onUkClick);
 
-const genresEn = [
-  { id: 28, name: 'Action' },
-  { id: 12, name: 'Adventure' },
-  { id: 16, name: 'Animation' },
-  { id: 35, name: 'Comedy' },
-  { id: 80, name: 'Crime' },
-  { id: 99, name: 'Documentary' },
-  { id: 18, name: 'Drama' },
-  { id: 10751, name: 'Family' },
-  { id: 14, name: 'Fantasy' },
-  { id: 36, name: 'History' },
-  { id: 27, name: 'Horror' },
-  { id: 10402, name: 'Music' },
-  { id: 9648, name: 'Mystery' },
-  { id: 10749, name: 'Romance' },
-  { id: 878, name: 'Science Fiction' },
-  { id: 10770, name: 'TV Movie' },
-  { id: 53, name: 'Thriller' },
-  { id: 10752, name: 'War' },
-  { id: 37, name: 'Western' },
-];
+// const genresEn = [
+//   { id: 28, name: 'Action' },
+//   { id: 12, name: 'Adventure' },
+//   { id: 16, name: 'Animation' },
+//   { id: 35, name: 'Comedy' },
+//   { id: 80, name: 'Crime' },
+//   { id: 99, name: 'Documentary' },
+//   { id: 18, name: 'Drama' },
+//   { id: 10751, name: 'Family' },
+//   { id: 14, name: 'Fantasy' },
+//   { id: 36, name: 'History' },
+//   { id: 27, name: 'Horror' },
+//   { id: 10402, name: 'Music' },
+//   { id: 9648, name: 'Mystery' },
+//   { id: 10749, name: 'Romance' },
+//   { id: 878, name: 'Science Fiction' },
+//   { id: 10770, name: 'TV Movie' },
+//   { id: 53, name: 'Thriller' },
+//   { id: 10752, name: 'War' },
+//   { id: 37, name: 'Western' },
+// ];
 
-const genresUkr = [
-  { id: 28, name: 'Бойовик' },
-  { id: 12, name: 'Пригоди' },
-  { id: 16, name: 'Мультфільм' },
-  { id: 35, name: 'Комедія' },
-  { id: 80, name: 'Кримінал' },
-  { id: 99, name: 'Документальний' },
-  { id: 18, name: 'Драма' },
-  { id: 10751, name: 'Сімейний' },
-  { id: 14, name: 'Фентезі' },
-  { id: 36, name: 'Історичний' },
-  { id: 27, name: 'Жахи' },
-  { id: 10402, name: 'Музика' },
-  { id: 9648, name: 'Детектив' },
-  { id: 10749, name: 'Мелодрама' },
-  { id: 878, name: 'Фантастика' },
-  { id: 10770, name: 'Телефільм' },
-  { id: 53, name: 'Трилер' },
-  { id: 10752, name: 'Військовий' },
-  { id: 37, name: 'Вестерн' },
-];
+// const genresUkr = [
+//   { id: 28, name: 'Бойовик' },
+//   { id: 12, name: 'Пригоди' },
+//   { id: 16, name: 'Мультфільм' },
+//   { id: 35, name: 'Комедія' },
+//   { id: 80, name: 'Кримінал' },
+//   { id: 99, name: 'Документальний' },
+//   { id: 18, name: 'Драма' },
+//   { id: 10751, name: 'Сімейний' },
+//   { id: 14, name: 'Фентезі' },
+//   { id: 36, name: 'Історичний' },
+//   { id: 27, name: 'Жахи' },
+//   { id: 10402, name: 'Музика' },
+//   { id: 9648, name: 'Детектив' },
+//   { id: 10749, name: 'Мелодрама' },
+//   { id: 878, name: 'Фантастика' },
+//   { id: 10770, name: 'Телефільм' },
+//   { id: 53, name: 'Трилер' },
+//   { id: 10752, name: 'Військовий' },
+//   { id: 37, name: 'Вестерн' },
+// ];
 
-let genresAll = { genresEn, genresUkr };
+// let genresAll = { genresEn, genresUkr };
 // console.log(genresAll.genresUkr);
 let genres = genresAll.genresEn;
 
