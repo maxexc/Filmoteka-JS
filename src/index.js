af3ab13697c1d591202eb92b194ea4a148196121
Maksym Solomko <max.exc7@gmail.com>
2022-11-01 21:37:10 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import genresAll from './js/genres.js';
+import movieCard from './templates/card.hbs';
+
+// console.log(genresAll);
+
+console.dir(movieCard());
+let genres = genresAll.genresEn;
+
 const gallery = document.querySelector('.gallery');
 const btnEn = document.querySelector('#en');
 const btnUk = document.querySelector('#uk');
@@ -8,12 +16,16 @@ btnUk.addEventListener('click', onUkClick);
 function onEnClick() {
   gallery.innerHTML = '';
   fetchApi('en');
+  fetchGenres('en');
   localStorage.setItem('language', 'en');
+  genres = genresAll.genresEn;
 }
 function onUkClick() {
   gallery.innerHTML = '';
   fetchApi('uk');
+  fetchGenres('uk');
   localStorage.setItem('language', 'uk');
+  genres = genresAll.genresUkr;
 }
 
 const currentLanguage = localStorage.getItem('language');
@@ -41,27 +53,27 @@ async function fetchGenres(language) {
 
 // console.log(genres);
 
-const genres = [
-  { id: 28, name: 'Action' },
-  { id: 12, name: 'Adventure' },
-  { id: 16, name: 'Animation' },
-  { id: 35, name: 'Comedy' },
-  { id: 80, name: 'Crime' },
-  { id: 99, name: 'Documentary' },
-  { id: 18, name: 'Drama' },
-  { id: 10751, name: 'Family' },
-  { id: 14, name: 'Fantasy' },
-  { id: 36, name: 'History' },
-  { id: 27, name: 'Horror' },
-  { id: 10402, name: 'Music' },
-  { id: 9648, name: 'Mystery' },
-  { id: 10749, name: 'Romance' },
-  { id: 878, name: 'Science Fiction' },
-  { id: 10770, name: 'TV Movie' },
-  { id: 53, name: 'Thriller' },
-  { id: 10752, name: 'War' },
-  { id: 37, name: 'Western' },
-];
+// const genres = [
+//   { id: 28, name: 'Action' },
+//   { id: 12, name: 'Adventure' },
+//   { id: 16, name: 'Animation' },
+//   { id: 35, name: 'Comedy' },
+//   { id: 80, name: 'Crime' },
+//   { id: 99, name: 'Documentary' },
+//   { id: 18, name: 'Drama' },
+//   { id: 10751, name: 'Family' },
+//   { id: 14, name: 'Fantasy' },
+//   { id: 36, name: 'History' },
+//   { id: 27, name: 'Horror' },
+//   { id: 10402, name: 'Music' },
+//   { id: 9648, name: 'Mystery' },
+//   { id: 10749, name: 'Romance' },
+//   { id: 878, name: 'Science Fiction' },
+//   { id: 10770, name: 'TV Movie' },
+//   { id: 53, name: 'Thriller' },
+//   { id: 10752, name: 'War' },
+//   { id: 37, name: 'Western' },
+// ];
 
 // let ids = 37;
 // console.log(ids);
